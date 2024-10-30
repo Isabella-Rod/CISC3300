e32743b519238652030d0b080d6dcf851fa5c487
Isabella-Rod <irodrigues3@fordham.edu>
2024-10-30 16:44:17 -0400

    Update index.php

--- Homework 7/index.php
+++ Homework 7/index.php
@@ -1,13 +1,12 @@
 <?php
 require_once 'controllers/UserController.php';
-require_once 'models/UserModel.php';
 
 $userController = new UserController();
 
 $requestUri = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-if ($requestUri === '/users' && $requestMethod === 'GET') {
+if ($requestMethod === 'GET' && strpos($requestUri, 'action=json') == true) {
     $userController->getUsersJson();
 } else {
     $userController->showUsersPage();
